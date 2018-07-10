f17507fc7bedc84247f2b308dd27894695e01ca1
Brandon <brandon@reddirtwebdesign.com>
2018-07-10 00:48:24 -0700

    feat: NavBar
    Created NavBar Component, Added it to the homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import AppNavBar from '../src/components/AppNavbar'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello</h1>
+        <AppNavBar />
       </div>
     );
   }
---
+++ client/src/components/AppNavbar.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import {
+  collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container,
+  Collapse
+} from 'reactstrap'
+
+class AppNavBar extends Component {
+
+  state = { isOpen: true }
+
+  toggle = () => {
+    this.setState({ isOpen: !this.state.isOpen });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="https://github.com/brandonsmith8038">
+                    GitHub
+                </NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavBar;
